0f3022f753fb926b1b16101ed00cbcc8e279ff1c
VVWebflow <45698097+Vonky13@users.noreply.github.com>
2024-11-25 22:32:04 +0100

    shop.html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,124 @@
 
-// Main
+const allWatches = [
+    {
+        Name: 'Space Timer Jupiter Gold',
+        Serial: 'CH-9341.2-CUBK',
+        Image: 'Jupiter Gold',
+        Price: '$29,900',
+        Limited: true
+    },
+    {
+        Name: 'Arnold & Son Time Pyramid',
+        Serial: 'AS001',
+        Price: '$25,000',
+        Image: 'Arnold & Son Time Pyramid',
+        Limited: true
+    },
+    {
+        Name: 'Breguet Type XX',
+        Serial: 'BG020',
+        Price: '$18,500',
+        Image: 'Breguet Type XX',
+        Limited: false
+    },
+    {
+        Name: 'Breitling Superocean',
+        Serial: 'BR030',
+        Price: '$4,500',
+        Image: 'Breitling Superocean Automatic 42',
+        Limited: false
+    },
+    {
+        Name: 'Bremont Terra Nova',
+        Serial: 'BT040',
+        Price: '$7,500',
+        Image: 'Bremont Terra Nova',
+        Limited: false
+    },
+    {
+        Name: 'Bulova Super Seville',
+        Serial: 'BS050',
+        Price: '$2,500',
+        Image: 'Bulova Super Seville',
+        Limited: false
+    },
+    {
+        Name: 'Defy Skyline Americas',
+        Serial: 'DS060',
+        Price: '$10,000',
+        Image: 'Defy Skyline Americas',
+        Limited: true
+    },
+    {
+        Name: 'Longines Spirit Flyback',
+        Serial: 'LS070',
+        Price: '$3,200',
+        Image: 'Longines Spirit Flyback',
+        Limited: false
+    },
+    {
+        Name: 'NOMOS Tangente 2date',
+        Serial: 'NT080',
+        Price: '$3,500',
+        Image: 'NOMOS Tangente 2date',
+        Limited: false
+    },
+    {
+        Name: 'Original Spezialist SeaQ',
+        Serial: 'OS090',
+        Price: '$6,000',
+        Image: 'Original Spezialist SeaQ Chronograph',
+        Limited: false
+    },
+    {
+        Name: 'Shinola Monster The Lava Lake',
+        Serial: 'SM100',
+        Price: '$1,800',
+        Image: 'Shinola Monster The Lava Lake',
+        Limited: true
+    },
+    {
+        Name: 'Spezialist SeaQ Chronograph',
+        Serial: 'SS110',
+        Price: '$5,700',
+        Image: 'Spezialist SeaQ Chronograph',
+        Limited: false
+    },
+    {
+        Name: 'Chronoswiss SkelTec',
+        Serial: 'CH-9341.2-CUBK',
+        Price: '$22,000',
+        Image: 'Chronoswiss SkelTec',
+        Limited: true
+    },
+    {
+        Name: 'Ulysse Nardin Marine',
+        Serial: '1192-310-0A/1A',
+        Price: '$23,900',
+        Image: 'Ulysse Nardin Marine Torpilleur',
+        Limited: false
+    },
+    {
+        Name: 'Zenith Defy Skeleton',
+        Serial: '03.9300.3620/79.I001',
+        Price: '$11,000',
+        Image: 'Zenith Defy Skeleton',
+        Limited: false
+    },
+    {
+        Name: 'Hamilton Khaki',
+        Serial: 'H70215130',
+        Price: '$22,200',
+        Image: 'Titanium Auto',
+        Limited: false
+    }
+]
 
 const nav = [
     {
         PageName: 'Home',
         Link: '/index.html',
-        Active: true,
+        Active: false,
     },
     {
         PageName: 'Shop',
@@ -24,172 +137,272 @@ const nav = [
     }
 ]
 
-const navMenu = document.querySelector('#menu')
+let pathName = window.location.pathname
 
-let html = '<ul>'
+if(pathName == '/')
+    pathName = '/index.html'
 
-for(let element of nav)
+// Index
+
+if(pathName == '/index.html')
 {
-    html += `<li><a href="${element.Link}" class="${element.Active ? 'active-link' : ' '}">${element.PageName}</a></li>`
-}
+    nav[0].Active = true
 
-html += '</ul>'
+    // Main
 
-navMenu.innerHTML += html
+    const navMenu = document.querySelector('#menu')
 
-const mainWatch = {
-    Name: 'Space Timer Jupiter Gold',
-    Serial: 'CH-9341.2-CUBK',
-    Image: 'Main Watch'
-}
+    var html = '<ul>'
 
-const mainLeftSection = document.querySelector('#main-left')
-
-html = `<div><h2>${mainWatch.Serial}</h2><h1>${mainWatch.Name}</h1><a href='shop.html'>Find more</a></div>`
-mainLeftSection.innerHTML += html
-
-const mainRightSection = document.querySelector('#main-right')
-
-html = `<div><img alt='${mainWatch.Image}' src='assets/img/${mainWatch.Image}.png'/></div>`
-mainRightSection.innerHTML = html
-
-// New Arrivals
-
-const newArrivalsWatches = [
+    for(let element of nav)
     {
-        Name: 'Chronoswiss SkelTec',
+        html += `<li><a href="${element.Link}" class="${element.Active ? 'active-link' : ' '}">${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    navMenu.innerHTML += html
+
+    const mainWatch = {
+        Name: 'Space Timer Jupiter Gold',
         Serial: 'CH-9341.2-CUBK',
-        Price: '$22,000',
-        Image: 'Watch 1 Image',
-        Limited: true
-    },
-    {
-        Name: 'Ulysse Nardin Marine',
-        Serial: '1192-310-0A/1A',
-        Price: '$23,900',
-        Image: 'Watch 2 Image',
-        Limited: false
-    },
-    {
-        Name: 'Zenith Defy Skeleton',
-        Serial: '03.9300.3620/79.I001',
-        Price: '$11,000',
-        Image: 'Watch 3 Image',
-        Limited: false
-    },
-    {
-        Name: 'Hamilton Khaki',
-        Serial: 'H70215130',
-        Price: '$22,200',
-        Image: 'Watch 4 Image',
-        Limited: false
+        Image: 'Main Watch'
     }
-]
 
-const newArrivalsSection = document.querySelector('#newArrivals')
+    const mainLeftSection = document.querySelector('#main-left')
 
-html = `<div id='watches'>`
+    html = `<div><h2>${mainWatch.Serial}</h2><h1>${mainWatch.Name}</h1><a href='shop.html'>Find more</a></div>`
+    mainLeftSection.innerHTML += html
 
-for(watch of newArrivalsWatches)
-{
-    html += `<div class='watch'>`
-    if(watch.Limited)
-        html += `<div class='limited-edition'></div>`
+    const mainRightSection = document.querySelector('#main-right')
 
+    html = `<div><img alt='${mainWatch.Image}' src='assets/img/${mainWatch.Image}.png'/></div>`
+    mainRightSection.innerHTML = html
+
+    // New Arrivals
+
+    const newArrivalsWatches = allWatches.slice(allWatches.length-4,allWatches.length)
+
+    const newArrivalsSection = document.querySelector('#newArrivals')
+
+    html = `<div id='watches'>`
+
+    for(watch of newArrivalsWatches)
+    {
+        html += `<div class='watch'>`
+        if(watch.Limited)
+            html += `<div class='limited-edition'></div>`
+
+        
+                html += `<img src='assets/img/${watch.Image}.png' alt='${watch.Name}'/>
+                        <p>${watch.Serial}</p>
+                        <p>${watch.Name}</p>
+                        <p>${watch.Price}</p>
+                        <a href='/shop.html'>Buy now</a>
+                        </div>`
+    }
+
+    html += `</div>`
+
+    newArrivalsSection.innerHTML += html
+
+    // Counter
+
+    let counter = setInterval(function(){
+        const timeNow = new Date()
+        const endOfDiscount = new Date("December 31, 2024 20:00:00")
+        const timeLeft = endOfDiscount.getTime() - timeNow.getTime()
+
+        let secondMS = 1000
+        let minuteMS = secondMS*60
+        let hourMS = minuteMS*60
+        let daysMS = hourMS*24
+
+        let seconds = Math.floor(timeLeft % minuteMS / secondMS)
+        let minutes = Math.floor(timeLeft % hourMS / minuteMS)
+        let hours = Math.floor(timeLeft % daysMS / hourMS)
+        let days = Math.floor(timeLeft / daysMS)
+
+        const counterRight = document.querySelector('.counter-right')
+
+        html = `<div><h2>${days}</h2><p>Days</p></div><h3>:</h3>`
+        html += `<div><h2>${hours}</h2><p>Hours</p></div><h3>:</h3>`
+        html += `<div><h2>${minutes}</h2><p>Minutes</p></div><h3>:</h3>`
+        html += `<div><h2>${seconds}</h2><p>Seconds</p></div>`
+
+        counterRight.innerHTML = html
+    },1000)
+
+    // Your Ideal Watch
+
+    let idealWatchContent = [
+        {
+            Image: 'Men Watches',
+            Text: 'Watches For Men'
+        },
+        {
+            Image: 'Women Watches',
+            Text: 'Watches For Women'
+        },
+        {
+            Image: 'Kids Watches',
+            Text: 'Watches For Kids'
+        }
+    ]
+
+    const yourIdealWatchSection = document.querySelector('#yourIdealWatchContent')
+
+    html = ``
+
+    for(let element of idealWatchContent)
+    {
+        html += `<a href='shop.html'><div class='ideal-watch'><img src='assets/img/${element.Image}.jpg' alt=''/><h2>${element.Text}</h2></div></a>`
+    }
+
+    yourIdealWatchSection.innerHTML += html;
+
+    // Brands
+
+    const brandsSection = document.querySelector('#brands')
+
+    html = ''
+
+    const brandNames = ['Cartier','Rolex','Casio','Tissot','Longines']
+
+    for(let element of brandNames)
+    {
+        html += `<div class="brand-block"><img alt='${element}' src="assets/img/${element}.png"/></div>`
+    }
+
+    brandsSection.innerHTML += html
+
+    // Footer
+
+    const quickLinks = document.querySelector('#footer-links')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href='${element.Link}'>${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    quickLinks.innerHTML += html
+}
+
+// Shop
+
+else if(pathName == '/shop.html'){
+
+    nav[1].Active = true
+
+    const navMenu = document.querySelector('#menu')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href="${element.Link}" class="${element.Active ? 'active-link' : ' '}">${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    navMenu.innerHTML += html
+
+    const products = document.querySelector('#products')
+
+    html = ''
+
+    for(watch of allWatches)
+        {
+            html += `<div class='watch'>`
+            if(watch.Limited)
+                html += `<div class='limited-edition'></div>`
     
-            html += `<img src='assets/img/${watch.Image}.png' alt='${watch.Name}'/>
-                     <p>${watch.Serial}</p>
-                     <p>${watch.Name}</p>
-                     <p>${watch.Price}</p>
-                     <a href='/shop.html'>Buy now</a>
-                     </div>`
-}
+            
+                    html += `<img src='assets/img/${watch.Image}.png' alt='${watch.Name}'/>
+                            <p>${watch.Serial}</p>
+                            <p>${watch.Name}</p>
+                            <p>${watch.Price}</p>
+                            <a href='/contact.html'>Buy now</a>
+                            </div>`
+        }
 
-html += `</div>`
+    products.innerHTML = html
 
-newArrivalsSection.innerHTML += html
+    const quickLinks = document.querySelector('#footer-links')
 
-// Counter
+    html = '<ul>'
 
-let counter = setInterval(function(){
-    const timeNow = new Date()
-    const endOfDiscount = new Date("December 1, 2024 20:00:00")
-    const timeLeft = endOfDiscount.getTime() - timeNow.getTime()
-
-    let secondMS = 1000
-    let minuteMS = secondMS*60
-    let hourMS = minuteMS*60
-    let daysMS = hourMS*24
-
-    let seconds = Math.floor(timeLeft % minuteMS / secondMS)
-    let minutes = Math.floor(timeLeft % hourMS / minuteMS)
-    let hours = Math.floor(timeLeft % daysMS / hourMS)
-    let days = Math.floor(timeLeft / daysMS)
-
-    const counterRight = document.querySelector('.counter-right')
-
-    html = `<div><h2>${days}</h2><p>Days</p></div><h3>:</h3>`
-    html += `<div><h2>${hours}</h2><p>Hours</p></div><h3>:</h3>`
-    html += `<div><h2>${minutes}</h2><p>Minutes</p></div><h3>:</h3>`
-    html += `<div><h2>${seconds}</h2><p>Seconds</p></div>`
-
-    counterRight.innerHTML = html
-},1000)
-
-// Your Ideal Watch
-
-let idealWatchContent = [
+    for(let element of nav)
     {
-        Image: 'Men Watches',
-        Text: 'Watches For Men'
-    },
-    {
-        Image: 'Women Watches',
-        Text: 'Watches For Women'
-    },
-    {
-        Image: 'Kids Watches',
-        Text: 'Watches For Kids'
+        html += `<li><a href='${element.Link}'>${element.PageName}</a></li>`
     }
-]
 
-const yourIdealWatchSection = document.querySelector('#yourIdealWatchContent')
+    html += '</ul>'
 
-html = ``
-
-for(let element of idealWatchContent)
-{
-    html += `<a href='shop.html'><div class='ideal-watch'><img src='assets/img/${element.Image}.jpg' alt=''/><h2>${element.Text}</h2></div></a>`
+    quickLinks.innerHTML += html
 }
 
-yourIdealWatchSection.innerHTML += html;
-
-// Brands
-
-const brandsSection = document.querySelector('#brands')
-
-html = ''
-
-const brandNames = ['Cartier','Rolex','Casio','Tissot','Longines']
-
-for(let element of brandNames)
+else if(pathName == '/contact.html')
 {
-    html += `<div class="brand-block"><img alt='${element}' src="assets/img/${element}.png"/></div>`
+    nav[2].Active = true
+
+    const navMenu = document.querySelector('#menu')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href="${element.Link}" class="${element.Active ? 'active-link' : ' '}">${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    navMenu.innerHTML += html
+
+    const quickLinks = document.querySelector('#footer-links')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href='${element.Link}'>${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    quickLinks.innerHTML += html
 }
 
-brandsSection.innerHTML += html
-
-// Footer
-
-const quickLinks = document.querySelector('#footer-links')
-
-html = '<ul>'
-
-for(let element of nav)
+else if(pathName == '/about-author.html')
 {
-    html += `<li><a href='${element.Link}'>${element.PageName}</a></li>`
+    nav[3].Active = true
+    const navMenu = document.querySelector('#menu')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href="${element.Link}" class="${element.Active ? 'active-link' : ' '}">${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    navMenu.innerHTML += html
+
+    const quickLinks = document.querySelector('#footer-links')
+
+    html = '<ul>'
+
+    for(let element of nav)
+    {
+        html += `<li><a href='${element.Link}'>${element.PageName}</a></li>`
+    }
+
+    html += '</ul>'
+
+    quickLinks.innerHTML += html
 }
-
-html += '</ul>'
-
-quickLinks.innerHTML += html
